cbd0f110245c4b434de7f802daf3f183ebc6e16e
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-18 09:05:51 -0400

    Add KV.Supervisor.init
    Cf. http://elixir-lang.org/getting-started/mix-otp/supervisor-and-application.html

--- kv/lib/kv/supervisor.ex
+++ kv/lib/kv/supervisor.ex
@@ -8,4 +8,12 @@ defmodule KV.Supervisor do
   @manager_name  KV.EventManager
   @registry_name KV.Registry
 
+  def init(:ok) do
+    children = [
+      worker(GenEvent,    [[name: @manager_name]]),
+      worker(KV.Registry, [@manager_name, [name: @registry_name]])
+    ]
+
+    supervise(children, strategy: :one_for_one)
+  end
 end
