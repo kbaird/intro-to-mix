21d947c432a6f25ebc1c2bc9160a9338e8f8e865
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-18 09:05:30 -0400

    Add KV.Supervisor.start_link
    Cf. http://elixir-lang.org/getting-started/mix-otp/supervisor-and-application.html

--- kv/lib/kv/supervisor.ex
+++ kv/lib/kv/supervisor.ex
@@ -1,6 +1,10 @@
 defmodule KV.Supervisor do
   use Supervisor
 
+  def start_link do
+    Supervisor.start_link(__MODULE__, :ok)
+  end
+
   @manager_name  KV.EventManager
   @registry_name KV.Registry
 
